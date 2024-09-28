73c3a6e793fedcd0763a56643c6b0d05927c16fe
BuildTools <unconfigured@null.spigotmc.org>
2024-09-28 23:38:53 +0200

    test

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -5,13 +5,14 @@ import React, { ReactNode, useState } from 'react'
 // Importa los componentes de las páginas que crearás en el futuro
 import FuturePage1 from './usertable'
 import FuturePage2 from './foodtable'
+import { PageProps } from '../../../../.next/types/app/layout'
 // ... importa más páginas según sea necesario
 
-export default function Page() {
+export default function Page({ children }: PageProps) {
   const [currentPage, setCurrentPage] = useState<ReactNode>(<FuturePage1 />)
 
   const handleNavClick = (page: string) => {
-    switch(page) {
+    switch (page) {
       case 'users':
         setCurrentPage(<FuturePage1 />)
         break
@@ -23,12 +24,14 @@ export default function Page() {
   }
 
   return (
-    <main className='flex flex-row'>
-        <AdminNavbar onNavClick={handleNavClick} />
-        <main className='w-full p-10'>
-          {currentPage}
-        </main>
+    <main className="flex flex-row">
+      <AdminNavbar onNavClick={handleNavClick} />
+      <main className="w-full p-10">
+        {currentPage}
+        {children} {/* Renderiza children si es necesario */}
+      </main>
     </main>
-  )
+  );
 }
+
 
